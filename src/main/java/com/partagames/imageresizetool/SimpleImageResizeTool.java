150d4569324ea95aaf3525eb4d99899ff8f5bcaa
Antti Kolehmainen <antionio@gmail.com>
2015-09-18 14:09:19 +0300

    Removed obsolete scale method

--- src/main/java/com/partagames/imageresizetool/SimpleImageResizeTool.java
+++ src/main/java/com/partagames/imageresizetool/SimpleImageResizeTool.java
@@ -94,28 +94,6 @@ public class SimpleImageResizeTool {
         }
     }
 
-    /**
-     * scale image
-     *
-     * @param sbi       image to scale
-     * @param imageType type of image
-     * @param dWidth    width of destination image
-     * @param dHeight   height of destination image
-     * @param fWidth    x-factor for transformation / scaling
-     * @param fHeight   y-factor for transformation / scaling
-     * @return scaled image
-     */
-    public static BufferedImage scale(BufferedImage sbi, int imageType, int dWidth, int dHeight, double fWidth, double fHeight) {
-        BufferedImage dbi = null;
-        if (sbi != null) {
-            dbi = new BufferedImage(dWidth, dHeight, imageType);
-            Graphics2D g = dbi.createGraphics();
-            AffineTransform at = AffineTransform.getScaleInstance(fWidth, fHeight);
-            g.drawRenderedImage(sbi, at);
-        }
-        return dbi;
-    }
-
     public static BufferedImage resizeImg(BufferedImage img, int newW, int newH) {
         int w = img.getWidth();
         int h = img.getHeight();
