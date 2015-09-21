3d438e16db2f3ffecbe14ba625344bbd625173af
Antti Kolehmainen <antionio@gmail.com>
2015-09-21 10:44:44 +0300

    Added license to source code

--- src/main/java/com/partagames/imageresizetool/Constants.java
+++ src/main/java/com/partagames/imageresizetool/Constants.java
@@ -1,3 +1,18 @@
+/*
+* Copyright 2015 Parta Games Oy
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.partagames.imageresizetool;
 
 import com.google.common.collect.ImmutableList;
@@ -9,14 +24,14 @@ public class Constants {
     public static final String VERSION = "0.0.1";
     public static final ImmutableList<String> OUTPUT_IMAGE_FORMATS = ImmutableList.of("png", "jpg", "gif");
     public static final ImmutableList<String> SUPPORTED_SCALING_HINTS = ImmutableList.of("n", "b");
-    
+
     // list the command line arguments
     public static final String ARG_DIMENSIONS_SHORT = "d";
     public static final String ARG_OUTPUT_SHORT = "o";
     public static final String ARG_FORMAT_SHORT = "f";
     public static final String ARG_HINT_SHORT = "s";
     public static final String ARG_HELP_SHORT = "h";
-    
+
     public static final String ARG_DIMENSIONS = "dimensions";
     public static final String ARG_OUTPUT = "output";
     public static final String ARG_FORMAT = "format";
